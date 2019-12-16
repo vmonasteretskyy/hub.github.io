cdd1f382c816898870a03deed0b1f84c4cfc40d4
mac <mac@maxs-MacBook-Pro.local>
2019-12-16 16:19:23 +0200

    ad

--- js/custom.js
+++ js/custom.js
@@ -1,26 +1,4 @@
-$(".brand_slider").slick({
-  centerMode: true,
-  infinite: true,
-  arrows: false,
-  variableWidth: true,
-  autoplay: true,
-  autoplaySpeed: 2000
-});
-$(".logo_slider").slick({
-  centerMode: true,
-  infinite: true,
-  arrows: false,
-  variableWidth: true,
-  autoplay: true,
-  autoplaySpeed: 2000
-});
-$(".menu_btn").click(function() {
-  $(".menu").css("left", "19px");
-});
-$(".close_btn").click(function() {
-  $(".menu").css("left", "-100vw");
-});
-
+// general srcipts
 function onlydesk() {
   if ($(window).width() > 767.9) {
     function go(target) {
@@ -36,18 +14,21 @@ function onlydesk() {
       });
     }
     $("#text_1").css("width", "100%");
-    go($("#text_2"));
-    go($("#text_3"));
-    go($("#text_4"));
+    if ($("main").hasClass("home")) {
+      go($("#text_2"));
+      go($("#text_3"));
+      go($("#text_4"));
+    }
   }
 }
+
 $(document).ready(function() {
-  onlydesk();
-});
-$(window).resize(function() {
-  onlydesk();
-});
-$(document).ready(function() {
+  $(".menu_btn").click(function() {
+    $(".menu").css("right", "20px");
+  });
+  $(".close_btn").click(function() {
+    $(".menu").css("right", "-100vw");
+  });
   function typeText(target, texttype) {
     var target;
     var targetPos = target.offset().top;
@@ -67,29 +48,63 @@ $(document).ready(function() {
       return executed;
     });
   }
-  typeText($("#type_1"), "Let us help you solve your problem");
-  typeText(
-    $("#type_2"),
-    "Search and Export Provider data based on NPI, Name, Location and Speciality"
-  );
+  // scrips for home page
+  if ($("main").hasClass("home")) {
+    $(".brand_slider").slick({
+      centerMode: true,
+      infinite: true,
+      arrows: false,
+      variableWidth: true,
+      autoplay: true,
+      autoplaySpeed: 2000
+    });
+    $(".logo_slider").slick({
+      centerMode: true,
+      infinite: true,
+      arrows: false,
+      variableWidth: true,
+      autoplay: true,
+      autoplaySpeed: 2000
+    });
+    // scroll mouse
+    $(".mouse").on("click", "a", function(event) {
+      //отменяем стандартную обработку нажатия по ссылке
+      event.preventDefault();
+
+      //забираем идентификатор бока с атрибута href
+      var id = $(this).attr("href"),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+        top = $(id).offset().top - 110;
+
+      //анимируем переход на расстояние - top за 1500 мс
+      $("body,html").animate({ scrollTop: top }, 1000);
+    });
+    typeText($("#type_1"), "Let us help you solve your problem");
+    typeText(
+      $("#type_2"),
+      "Search and Export Provider data based on NPI, Name, Location and Speciality"
+    );
+  } else if ($("main").hasClass("retrieval")) {
+    typeText($("#type_1"), "Let us know what you are looking for…");
+  } else if ($("main").hasClass("reporting")) {
+    typeText(
+      $("#type_1"),
+      "Like to build a Report? Click here to submit a request and someone will get in touch with you within 2 business days..."
+    );
+  } else if ($("main").hasClass("exchange")) {
+    typeText(
+      $("#type_1"),
+      "Like to know more about how FHIR can benefit you? Click here to submit a request and someone will get in touch with you in 1-2 business day(s) "
+    );
+  } else if ($("main").hasClass("automation")) {
+    typeText(
+      $("#type_1"),
+      "Like to know how your business process can be streamlined? Click here to submit a request and someone will get in touch with you in 1-2 business day(s)  "
+    );
+  }
+  onlydesk();
 });
 
-// scroll mouse
-$(".mouse").on("click", "a", function(event) {
-  //отменяем стандартную обработку нажатия по ссылке
-  event.preventDefault();
-
-  //забираем идентификатор бока с атрибута href
-  var id = $(this).attr("href"),
-    //узнаем высоту от начала страницы до блока на который ссылается якорь
-    top = $(id).offset().top - 110;
-
-  //анимируем переход на расстояние - top за 1500 мс
-  $("body,html").animate({ scrollTop: top }, 1000);
+$(window).resize(function() {
+  onlydesk();
 });
-// $("header li a").mouseover(function() {
-//   $(".drop").slideDown(300);
-// });
-// $("header li").mouseleave(function() {
-//   $(".drop").slideUp(300);
-// });
